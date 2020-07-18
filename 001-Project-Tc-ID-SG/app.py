66a657d523948d74bd349ecf8cc1eaa85c7e425b
serkangumus <serkangumus06@gmail.com>
2020-07-18 17:11:15 +0300

    001 Tc ID validation project

---
+++ 001-Project-Tc-ID-SG/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def tckimlik(n):
+    tc=[int(i) for i in str(n)]
+    sum1=(sum(tc[0:9:2]))*7 
+    mult=sum(tc[1:8:2])
+    result1=sum1-mult
+    if result1%10!=tc[9] and sum(tc[:10])%10!=tc[10]:
+        return 'not Valid'
+    else:
+        return 'Valid'
+
+@app.route('/', methods = ['GET'])
+def main_get():
+    return render_template('index.html', developer_name = 'Serkan', not_valid = False)
+
+
+@app.route('/', methods=['POST'])
+def TcKimlik():
+    alpha = request.form['tcnumber']
+    if not alpha.isdecimal():
+        return render_template('index.html', developer_name = 'Serkan', not_valid = True)
+    if len(alpha) != 11:
+        return render_template('index.html', developer_name = 'Serkan', not_valid = True)
+    number = int(alpha)
+    return render_template('result.html', tc_decimal = number, result = tckimlik(number), developer_name = 'Serkan')
+
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(debug=True)
+    #app.run('0.0.0.0',port=80)
